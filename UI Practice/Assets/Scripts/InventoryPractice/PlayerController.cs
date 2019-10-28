a4d324231324d3fcbcfd9f528a88ffff9f260a9a
lsw5955 <lsw5955@qq.com>
2019-10-29 07:05:58 +0800

    https://www.youtube.com/watch?v=DLAIYSMYy2g&t=68s

---
+++ UI Practice/Assets/Scripts/InventoryPractice/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if(moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
---
+++ UI Practice/Assets/Scripts/simpleUIPractice/ButtonExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonExample : MonoBehaviour
+{
+    public void Hello()
+    {
+        Debug.Log("Hello!!");
+    }
+}
