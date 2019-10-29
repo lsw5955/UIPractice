10b2525699525b28420ce0e1bbb402d1852d86d3
铁拳无敌俞大猷 <lishiwen@qq.com>
2019-10-29 16:42:44 +0800

    https://www.youtube.com/watch?v=OG7vHstkZqM

---
+++ UI Practice/Assets/Scripts/InventoryPractice/Slot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    private Inventory inventory;
+    public int indexOfSlot;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void Update()
+    {
+        if(transform.childCount <= 0)
+        {
+            inventory.isFull[indexOfSlot] = false;
+        }
+    }
+
+    public void DropItem()
+    {
+        foreach (Transform child in transform)
+        {
+            child.GetComponent<Spawn>().SpawnDroppedItem();
+            Destroy(child.gameObject);
+        }
+    }
+}
---
+++ UI Practice/Assets/Scripts/InventoryPractice/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject item;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    public void SpawnDroppedItem()
+    {
+        Vector2 itemPos = new Vector2(player.position.x, player.position.y + 5);
+        Instantiate(item, itemPos, Quaternion.identity);
+    }
+}
